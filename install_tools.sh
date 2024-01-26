74cd5e8857515fe9af3ce1892249b05c6f123f01
bellencoATeviden <111117142+bellencoATatos@users.noreply.github.com>
2024-01-26 11:08:35 +0100

    MAJ

--- install_tools.sh
+++ install_tools.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+yum -y groupinstall "Development Tools"
+yum -y install unzip
+tar xvfz /tmp/TAR/inxi.tar.gz -C /opt
+cd /tmp
+unzip /tmp/TAR/darshan.zip 
+cd darshan-main
+./prepare.sh 
+./configure --prefix=/opt/darshan 
+make -j 2
+make install
+sleep 10
+cd ..
+tar xvfz /tmp/TAR/selfie-1.0.4.tar.gz
+cd selfie-1.0.4
+./autogen.sh 
+./configure
+
+
 
